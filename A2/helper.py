3f18023a73413cc8fc6dfcfa6fdf208c469978f7
Jenny Cao <yuanhui.cao@mail.mcgill.ca>
2021-09-26 18:54:36 -0400

    Refactored and tested functions, also added a bit for q2b;

--- A2/helper.py
+++ A2/helper.py
@@ -1,4 +1,35 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
+#Q1 Helper functions
+def convertIndexToNames(index):
+    dataFrame = pd.read_table("email-Enron/addresses-email-Enron.txt", 
+                names=['Index', 'Email Address'])
+    mostImportantEmail = dataFrame[dataFrame['Index'].isin(index)]
+    mostImportantEmail = mostImportantEmail['Email Address']
+    return np.asarray(mostImportantEmail)
+
+
+def postProcessing(dictCentrality):
+    rankings = np.asarray(list(dictCentrality.values()))
+    mostPopularIdx = np.argsort((-1) * rankings) # rank from highest to lowest
+    mostPopularIdx = mostPopularIdx[0:5]
+    mostImportantEmail = convertIndexToNames(mostPopularIdx)
+    highestCentrality = rankings[mostPopularIdx]
+    return (mostImportantEmail, highestCentrality)
+
+def printRankingResults(centralityMeasure, mostImportantEmail, highestCentrality):
+    print(centralityMeasure + ': ')
+    for i in range(5):
+        print('Ranking: ' + str(i+1) + ';   Email: ' + mostImportantEmail[i] + 
+        ';  ' + centralityMeasure + ':  '+ str(highestCentrality[i]))
+    print('')
+
+
+#Q2 Helper functions
 def convertFrozenset2Dict(partitionList):
     sets = [list(x) for x in partitionList]
     partition = {}
@@ -12,5 +43,16 @@ def convertLabel2Dict(labels):
     partition = {}
     for nodeIdx in range(1, len(labels)+1):
         partition[str(nodeIdx)] = labels[nodeIdx-1]
-    print(partition)
     return partition
+
+def partitionVisualization(G, partition, methodName, colorSet, plotLabel=True):
+    pos = nx.spring_layout(G)
+    # color the nodes according to their partition
+    cmap = cm.get_cmap(colorSet, max(partition.values()) + 1)
+    nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=400,
+                       cmap=cmap, node_color=list(partition.values()))
+    nx.draw_networkx_edges(G, pos, width=2.0, alpha=0.5)
+    if plotLabel:
+        nx.draw_networkx_labels(G, pos)
+    plt.title(methodName, fontsize=16)
+    plt.show()
--- A2/main.py
+++ A2/main.py
@@ -8,26 +8,23 @@ from community_Q2 import *
 from helper import *
 
 
-#Raw data
-emailsList = np.loadtxt("email-Enron/email-Enron.txt", dtype=int)
-# print(np.delete(emailsList, 2, 1))
-# print(emailsList)
-# print([emailsList[:, 0],emailsList[:, 1]])
-# print(createInitialGraph(emails))
-emailGraph = generateCSC(createUniqueEdges(np.delete(emailsList, 2, 1)))
+#Processing data
 
-strikeGraph = nx.read_gml('real-classic/strike.gml')
+# emailsList = np.loadtxt("email-Enron/email-Enron.txt", dtype=int)
+# emailGraph = generateCSC(createUniqueEdges(np.delete(emailsList, 2, 1)))
+
+# strikeGraph = nx.read_gml('real-classic/strike.gml')
 karateGraph = nx.read_gml('real-classic/karate.gml')
-polbooksGraph = nx.read_gml('real-classic/polbooks.gml')
+# polbooksGraph = nx.read_gml('real-classic/polbooks.gml')
 #polblogsGraph = nx.read_gml('real-classic/polblogs.gml')
-#footballGraph = nx.read_gml('real-classic/football.gml')
+# #footballGraph = nx.read_gml('real-classic/football.gml')
 
-coraDict = np.load('real-node-label/ind.cora.graph', allow_pickle=True)
-coraGraph = nx.Graph(dict(coraDict))
-citeseerDict = np.load('real-node-label/ind.citeseer.graph', allow_pickle=True)
-citeseerGraph = nx.Graph(dict(citeseerDict))
-pubmedDict = np.load('real-node-label/ind.pubmed.graph', allow_pickle=True)
-pubmedGraph = nx.Graph(dict(pubmedDict))
+# coraDict = np.load('real-node-label/ind.cora.graph', allow_pickle=True)
+# coraGraph = nx.Graph(dict(coraDict))
+# citeseerDict = np.load('real-node-label/ind.citeseer.graph', allow_pickle=True)
+# citeseerGraph = nx.Graph(dict(citeseerDict))
+# pubmedDict = np.load('real-node-label/ind.pubmed.graph', allow_pickle=True)
+# pubmedGraph = nx.Graph(dict(pubmedDict))
 
 #########
 
@@ -38,10 +35,18 @@ pubmedGraph = nx.Graph(dict(pubmedDict))
 #Q2: Community detection on graphsß
 #real-classic datasets
 # communityDetection(strikeGraph)
-# communityDetection(karateGraph)
+communityDetection(karateGraph)
 # communityDetection(polbooksGraph, plotLabel=False) 
 # communityDetection(polblogsGraph)  # DUPLICATED EDGE ERROR? 
 # communityDetection(footballGraph)  # DUPLICATED EDGE ERROR?
 
 #real-node-label
-communityDetection(coraGraph, plotLabel=False)
+# communityDetection(coraGraph, plotLabel=False)
+# communityDetection(citeseerGraph, plotLabel=False)
+# communityDetection(pubmedGraph, plotLabel=False)
+
+#LFR
+# LFRGraph = nx.generators.community.LFR_benchmark_graph(1000, 3, 1.5, 0.5, average_degree=5, min_community=20, seed=3)
+# communityDetection(LFRGraph, plotLabel=False)
+
+#Performance
