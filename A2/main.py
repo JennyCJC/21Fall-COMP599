af80a6906e0ee38620e6ce38ea4386fb7ec5eb52
Jenny Cao <yuanhui.cao@mail.mcgill.ca>
2021-10-02 15:51:49 -0400

    Finish Performance Functions;

--- A2/main.py
+++ A2/main.py
@@ -14,9 +14,9 @@ from helper import *
 # emailGraph = generateCSC(createUniqueEdges(np.delete(emailsList, 2, 1)))
 
 # strikeGraph = nx.read_gml('real-classic/strike.gml')
-karateGraph = nx.read_gml('real-classic/karate.gml')
+# karateGraph = nx.read_gml('real-classic/karate.gml')
 # polbooksGraph = nx.read_gml('real-classic/polbooks.gml')
-#polblogsGraph = nx.read_gml('real-classic/polblogs.gml')
+# polblogsGraph = nx.read_gml('real-classic/polblogs.gml')
 # #footballGraph = nx.read_gml('real-classic/football.gml')
 
 # coraDict = np.load('real-node-label/ind.cora.graph', allow_pickle=True)
@@ -26,6 +26,17 @@ karateGraph = nx.read_gml('real-classic/karate.gml')
 # pubmedDict = np.load('real-node-label/ind.pubmed.graph', allow_pickle=True)
 # pubmedGraph = nx.Graph(dict(pubmedDict))
 
+# coraLabel = np.load('real-node-label/ind.cora.ally', allow_pickle=True)
+# coraTestIndex = np.loadtxt('real-node-label/ind.cora.test.index')
+# coraOrderedLabels = np.nonzero(coraLabel)[1]
+
+# citeseerLabel = np.load('real-node-label/ind.citeseer.ally', allow_pickle=True)
+# citeseerTestIndex = np.loadtxt('real-node-label/ind.citeseer.test.index')
+# citeseerOrderedLabels = np.nonzero(citeseerLabel)[1]
+
+# pubmedLabel = np.load('real-node-label/ind.pubmed.ally', allow_pickle=True)
+# pubmedTestIndex = np.loadtxt('real-node-label/ind.pubmed.test.index')
+# pubmedOrderedLabels = np.nonzero(pubmedLabel)[1]
 #########
 
 
@@ -34,20 +45,29 @@ karateGraph = nx.read_gml('real-classic/karate.gml')
 
 #Q2: Community detection on graphsß
 #real-classic datasets
-# communityDetection(strikeGraph)
-karatePredictions = communityDetection(karateGraph)
-# communityDetection(polbooksGraph, plotLabel=False) 
+# strikePredictions = communityDetection(strikeGraph)
+# karatePredictions = communityDetection(karateGraph)
+# polbooksPredictions = communityDetection(polbooksGraph, plotLabel=False) 
 # communityDetection(polblogsGraph)  # DUPLICATED EDGE ERROR? 
 # communityDetection(footballGraph)  # DUPLICATED EDGE ERROR?
 
 #real-node-label
-# communityDetection(coraGraph, plotLabel=False)
-# communityDetection(citeseerGraph, plotLabel=False)
-# communityDetection(pubmedGraph, plotLabel=False)
+# coraPredictions = communityDetection(coraGraph, plotLabel=False)
+# citeseerPredictions = communityDetection(citeseerGraph, plotLabel=False)
+# pubmedPredictions = communityDetection(pubmedGraph, plotLabel=False)
 
 #LFR
-# LFRGraph = nx.generators.community.LFR_benchmark_graph(1000, 3, 1.5, 0.5, average_degree=5, min_community=20, seed=3)
-# communityDetection(LFRGraph, plotLabel=False)
+LFRGraph = nx.generators.community.LFR_benchmark_graph(1000, 3, 1.5, 0.5, average_degree=5, min_community=20, seed=3)
+LFRPredictions = communityDetection(LFRGraph, plotLabel=False)
+LFRcommunities = getUniqueCommunities(LFRGraph)
+LFRLableDict = getLabelDictFromCommunities(LFRcommunities)
+
+
 
 #Performance
-overallPerformance(karateGraph, karatePredictions)
+# overallPerformance(karateGraph, karatePredictions)
+# overallPerformance(strikeGraph, strikePredictions)
+# overallPerformance(polbooksGraph, polbooksPredictions)
+# overallPerformance(coraGraph, coraPredictions, removeUnlabeled=coraTestIndex, truthLabels=coraOrderedLabels)
+# overallPerformance(citeseerGraph, citeseerPredictions, removeUnlabeled=citeseerTestIndex, truthLabels=citeseerOrderedLabels)
+overallPerformance(LFRGraph, LFRPredictions, truthLabels=getLabels(LFRLableDict))
